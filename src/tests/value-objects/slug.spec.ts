a5224049cd4c0fd9d7d501c6e74a78096d935553
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-17 13:52:54 -0300

    test: slug value object

---
+++ src/tests/value-objects/slug.spec.ts
@@ -0,0 +1,9 @@
+import { Slug } from "../../domain/entities/value-objects/slug";
+import { expect, test } from "vitest";
+
+
+test('it should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Example question title')
+
+  expect(slug.value).toEqual('example-question-title')
+})
