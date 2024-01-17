b7b456495fda3f30e422b53644522167e47bb9ff
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-17 13:50:15 -0300

    feat: create value object to slug

--- src/domain/entities/interfaces/IQuestionProps.ts
+++ src/domain/entities/interfaces/IQuestionProps.ts
@@ -1,5 +1,8 @@
+import { Slug } from "../value-objects/slug"
+
 export interface IQuestionProps {
   title: string
+  slug: Slug
   content: string
   authorId: string
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,11 @@
 import { randomUUID } from "node:crypto"
 import { IQuestionProps } from "./interfaces/IQuestionProps"
+import { Slug } from "./value-objects/slug"
 
 export class Question {
   public id?: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
 
@@ -11,6 +13,7 @@ export class Question {
     this.title = props.title
     this.content = props.content
     this.authorId = props.authorId
+    this.slug = props.slug
     this.id = id ?? randomUUID()
   }
 }
