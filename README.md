351c7f31adbde17f69cd62e4d76bc1d26180eb56
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-19 14:55:20 -0300

    feat: create question use case

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ddd-node
+# ddd-node (in development)
 Forum app build using Node, Typescript, Prisma and applying Domain Driven Design and Clean Architecture principles
 
 This app will be completed on [nest-clean](https://github.com/ArthurPMachado/nest-clean)
---
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -0,0 +1,29 @@
+import {
+  ICreateQuestionUseCaseRequest,
+  ICreateQuestionUseCaseResponse,
+} from './interfaces/IQuestionUseCase'
+import { IQuestionsRepository } from '../repositories/interfaces/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export class CreateQuestionUseCase {
+  constructor(private questionRepository: IQuestionsRepository) {}
+
+  async execute({
+    authorId,
+    title,
+    content,
+  }: ICreateQuestionUseCaseRequest): Promise<ICreateQuestionUseCaseResponse> {
+    const question = Question.create({
+      authorId: new UniqueEntityID(authorId),
+      title,
+      content,
+    })
+
+    await this.questionRepository.create(question)
+
+    return {
+      question,
+    }
+  }
+}
