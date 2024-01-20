b7f4bb5e3f06fa82c6f59721c5e433a224233dae
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-20 15:59:11 -0300

    test: delete question

--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -8,12 +8,17 @@ export class DeleteQuestionUseCase {
   constructor(private questionRepository: IQuestionsRepository) {}
 
   async execute({
+    authorId,
     questionId,
   }: IDeleteQuestionUseCaseRequest): Promise<IDeleteQuestionUseCaseResponse> {
     const question = await this.questionRepository.findById(questionId)
 
     if (!question) {
       throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
     }
 
     await this.questionRepository.delete(question)
--- src/domain/forum/application/use-cases/interfaces/IDeleteQuestionUseCase.ts
+++ src/domain/forum/application/use-cases/interfaces/IDeleteQuestionUseCase.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-interface */
 
 export interface IDeleteQuestionUseCaseRequest {
+  authorId: string
   questionId: string
 }
 
