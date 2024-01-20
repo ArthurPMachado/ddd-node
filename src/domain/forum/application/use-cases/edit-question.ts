bdf252016aa667661d554ff2d116febdec98b4e3
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-20 18:13:55 -0300

    feat: edit question use case

---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,33 @@
+import { IQuestionsRepository } from '../repositories/interfaces/questions-repository'
+import {
+  IEditQuestionUseCaseRequest,
+  IEditQuestionUseCaseResponse,
+} from './interfaces/IEditQuestionUseCase'
+
+export class EditQuestionUseCase {
+  constructor(private questionRepository: IQuestionsRepository) {}
+
+  async execute({
+    authorId,
+    questionId,
+    title,
+    content,
+  }: IEditQuestionUseCaseRequest): Promise<IEditQuestionUseCaseResponse> {
+    const question = await this.questionRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    question.title = title
+    question.content = content
+
+    await this.questionRepository.save(question)
+
+    return {}
+  }
+}
---
+++ src/domain/forum/application/use-cases/interfaces/IEditQuestionUseCase.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+export interface IEditQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  title: string
+  content: string
+}
+
+export interface IEditQuestionUseCaseResponse {}
