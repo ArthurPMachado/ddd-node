ffc387da05825a0cbfb6ef85a6406c285cc5ecb0
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-20 18:23:25 -0300

    feat: edit answer use case

---
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -0,0 +1,31 @@
+import { IAnswersRepository } from '../repositories/interfaces/answers-repository'
+import {
+  IEditAnswerUseCaseRequest,
+  IEditAnswerUseCaseResponse,
+} from './interfaces/IEditAnswerUseCase'
+
+export class EditAnswerUseCase {
+  constructor(private answerRepository: IAnswersRepository) {}
+
+  async execute({
+    authorId,
+    answerId,
+    content,
+  }: IEditAnswerUseCaseRequest): Promise<IEditAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId)
+
+    if (!answer) {
+      throw new Error('Answer not found.')
+    }
+
+    if (authorId !== answer.authorId.toString()) {
+      throw new Error('Not allowed.')
+    }
+
+    answer.content = content
+
+    await this.answerRepository.save(answer)
+
+    return {}
+  }
+}
---
+++ src/domain/forum/application/use-cases/interfaces/IEditAnswerUseCase.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+
+export interface IEditAnswerUseCaseRequest {
+  authorId: string
+  answerId: string
+  content: string
+}
+
+export interface IEditAnswerUseCaseResponse {}
