4d78d4db9156bb87e64365685262473fc9cf1a03
Arthur Machado <arthur.machado@iclubs.tech>
2024-01-22 02:17:48 -0300

    feat: create delete comment on answer use case

---
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -0,0 +1,29 @@
+import {
+  IDeleteAnswerCommentUseCaseRequest,
+  IDeleteAnswerCommentUseCaseResponse,
+} from './interfaces/IDeleteAnswerCommentUseCase'
+import { IAnswerCommentsRepository } from '../repositories/interfaces/answer-comments-repository'
+
+export class DeleteAnswerCommentUseCase {
+  constructor(private answerCommentsRepository: IAnswerCommentsRepository) {}
+
+  async execute({
+    authorId,
+    answerCommentId,
+  }: IDeleteAnswerCommentUseCaseRequest): Promise<IDeleteAnswerCommentUseCaseResponse> {
+    const answerComment =
+      await this.answerCommentsRepository.findById(answerCommentId)
+
+    if (!answerComment) {
+      throw new Error('Answer comment not found.')
+    }
+
+    if (answerComment.authorId.toString() !== authorId) {
+      throw new Error('Not allowed')
+    }
+
+    await this.answerCommentsRepository.delete(answerComment)
+
+    return {}
+  }
+}
---
+++ src/domain/forum/application/use-cases/interfaces/IDeleteAnswerCommentUseCase.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+export interface IDeleteAnswerCommentUseCaseRequest {
+  authorId: string
+  answerCommentId: string
+}
+
+export interface IDeleteAnswerCommentUseCaseResponse {}
